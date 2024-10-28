b20b2780bd22caa0269ef16beb69c7bfa2c3f786
Ivan Sentemon <sentemon.i@gmail.com>
2024-10-28 18:35:22 +0100

    Refactor IRepository interface and Repository implementation

--- backend/FitnessApp/src/PostService/PostService.Domain/Interfaces/IRepository.cs
+++ backend/FitnessApp/src/PostService/PostService.Domain/Interfaces/IRepository.cs
@@ -3,8 +3,8 @@ namespace PostService.Domain.Interfaces;
 public interface IRepository<TEntity> where TEntity : class
 {
     Task<IQueryable<TEntity>> GetAllAsync();
-    Task<TEntity> GetByIdAsync(Guid id);
-    Task<TEntity> AddAsync(TEntity entity);
-    Task<TEntity> UpdateAsync(TEntity entity);
-    Task<TEntity> DeleteAsync(Guid id);
+    Task<TEntity?> GetByIdAsync(Guid id);
+    Task<bool> AddAsync(TEntity entity);
+    Task<bool> UpdateAsync(TEntity entity);
+    Task<bool> DeleteAsync(Guid id);
 }

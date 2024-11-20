b5507709e9b987666daf51b4386f7d173ab65b9b
Ivan Sentemon <sentemon.i@gmail.com>
2024-11-20 21:28:17 +0100

    Add sidebar component and integrate into app layout

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SideBarComponent } from "./core/sidebar/sidebar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SideBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })

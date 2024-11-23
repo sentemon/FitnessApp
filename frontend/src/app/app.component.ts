e79aecc4840cc6e30b32a4f1d8cf43758e2b33f5
Ivan Sentemon <sentemon.i@gmail.com>
2024-11-23 19:18:07 +0100

    Add MainComponent with template, and integrate into app layout

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { SideBarComponent } from "./shared/components/sidebar/sidebar.component";
+import { SideBarComponent } from "./shared/layout/sidebar/sidebar.component";
+import {MainComponent} from "./shared/layout/main/main.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SideBarComponent],
+  imports: [RouterOutlet, SideBarComponent, MainComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })

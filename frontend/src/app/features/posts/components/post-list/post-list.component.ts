5c3f88353821d28541e9489202bc78cdd0b42cb2
Ivan Sentemon <sentemon.i@gmail.com>
2024-11-28 18:30:09 +0100

    Add new posts with images and descriptions; update main layout height and sidebar overflow styles

--- frontend/src/app/features/posts/components/post-list/post-list.component.ts
+++ frontend/src/app/features/posts/components/post-list/post-list.component.ts
@@ -31,6 +31,30 @@ export class PostListComponent {
       title: "Title",
       username: "example",
       imageUrl: "assets/profile.svg"
+    },
+    {
+      commentCount: 34,
+      contentType: ContentType.Image,
+      contentUrl: "https://images.pexels.com/photos/863988/pexels-photo-863988.jpeg", // No Copyright
+      createdAt: new Date(),
+      description: "I love swimming!",
+      id: "215e4b41-b712-46e2-87a1-9fec01899c7d",
+      likeCount: 193,
+      title: "",
+      username: "example",
+      imageUrl: "assets/profile.svg"
+    },
+    {
+      commentCount: 91,
+      contentType: ContentType.Image,
+      contentUrl: "https://images.pexels.com/photos/248547/pexels-photo-248547.jpeg", // No Copyright
+      createdAt: new Date(),
+      description: "Yesterday's challenge was great",
+      id: "215e4b41-b712-46e2-87a1-9fec01899c7d",
+      likeCount: 393,
+      title: "Title",
+      username: "example",
+      imageUrl: "assets/profile.svg"
     }
   ];
 }

c8aab592d8a7afd468434d545ef9a2d247e37965
Ivan Sentemon <sentemon.i@gmail.com>
2024-11-29 19:04:07 +0100

    Implement post fetching in PostListComponent and mock data in PostService

--- frontend/src/app/features/posts/components/post-list/post-list.component.ts
+++ frontend/src/app/features/posts/components/post-list/post-list.component.ts
@@ -1,7 +1,8 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Post} from "../../models/post.model";
 import {ContentType} from "../../../../core/enums/content-type.enum";
 import {DatePipe, NgForOf, NgIf, NgOptimizedImage} from "@angular/common";
+import {PostService} from "../../services/post.service";
 
 @Component({
   selector: 'app-post-list',
@@ -15,46 +16,17 @@ import {DatePipe, NgForOf, NgIf, NgOptimizedImage} from "@angular/common";
   templateUrl: './post-list.component.html',
   styleUrl: './post-list.component.scss'
 })
-export class PostListComponent {
+export class PostListComponent implements OnInit{
+  constructor(private postService: PostService) { }
+
   protected readonly ContentType = ContentType;
 
-  // Mocks
-  posts: Post[] = [
-    {
-      commentCount: 4,
-      contentType: ContentType.Image,
-      contentUrl: "https://www.istockphoto.com/resources/images/PhotoFTLP/P5-NOV-iStock-2158268393.jpg", // No Copyright
-      createdAt: new Date(),
-      description: "the men play football",
-      id: "215e4b41-b712-46e2-87a1-9fec01899c7d",
-      likeCount: 56,
-      title: "Title",
-      username: "example",
-      imageUrl: "assets/profile.svg"
-    },
-    {
-      commentCount: 34,
-      contentType: ContentType.Image,
-      contentUrl: "https://images.pexels.com/photos/863988/pexels-photo-863988.jpeg", // No Copyright
-      createdAt: new Date(2024, 10, 20),
-      description: "I love swimming!",
-      id: "215e4b41-b712-46e2-87a1-9fec01899c7d",
-      likeCount: 193,
-      title: "",
-      username: "example",
-      imageUrl: "assets/profile.svg"
-    },
-    {
-      commentCount: 91,
-      contentType: ContentType.Image,
-      contentUrl: "https://images.pexels.com/photos/248547/pexels-photo-248547.jpeg", // No Copyright
-      createdAt: new Date(2024, 10, 19),
-      description: "Yesterday's challenge was great",
-      id: "215e4b41-b712-46e2-87a1-9fec01899c7d",
-      likeCount: 393,
-      title: "Title",
-      username: "example",
-      imageUrl: "assets/profile.svg"
-    }
-  ];
+  posts: Post[] = [];
+
+  ngOnInit(): void {
+    this.postService.getAllPosts().subscribe(posts => {
+      this.posts = posts;
+    });
+  }
+  
 }

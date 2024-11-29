9c3ebed88b7cafd0feb86a3cf1adadfb23aab3a3
Ivan Sentemon <sentemon.i@gmail.com>
2024-11-29 22:13:06 +0100

    Refactor Comment and Like components for improved interactivity; add like toggle functionality and update styles

--- frontend/src/app/features/posts/components/like/like.component.ts
+++ frontend/src/app/features/posts/components/like/like.component.ts
@@ -1,13 +1,27 @@
 import {Component, Input} from '@angular/core';
 import {Post} from "../../models/post.model";
+import {NgClass} from "@angular/common";
 
 @Component({
   selector: 'app-like',
   standalone: true,
-  imports: [],
+  imports: [
+    NgClass
+  ],
   templateUrl: './like.component.html',
   styleUrl: './like.component.scss'
 })
 export class LikeComponent {
   @Input() post!: Post;
+  isLiked: boolean = false;
+
+  userHasLiked(post: Post): boolean {
+    return this.isLiked;
+  }
+
+  toggleLike(post: Post): void {
+    this.isLiked = !this.isLiked;
+
+    post.likeCount += this.isLiked ? 1 : -1;
+  }
 }

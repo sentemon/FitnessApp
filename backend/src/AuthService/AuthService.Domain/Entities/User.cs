ec2157491b25ef8fc415955d6cb4eb307ffb9312
Ivan Sentemon <sentemon.i@gmail.com>
2024-12-10 15:29:42 +0100

    Add User entity to AuthService domain

---
+++ backend/src/AuthService/AuthService.Domain/Entities/User.cs
@@ -0,0 +1,25 @@
+namespace AuthService.Domain.Entities;
+
+public class User
+{
+    public Guid Id { get; private set; }
+    public string ExternalId { get; private set; }
+    public string FirstName { get; private set; }
+    public string LastName { get; private set; }
+    public string Username { get; set; }
+    public string Email { get; private set; }
+    public string ImageUrl { get; private set; }
+    public DateTime CreatedAt { get; private set; }
+
+    public User(string externalId, string firstName, string lastName, string username, string email, string imageUrl)
+    {
+        Id = Guid.NewGuid();
+        ExternalId = externalId;
+        FirstName = firstName;
+        LastName = lastName;
+        Username = username;
+        Email = email;
+        ImageUrl = imageUrl;
+        CreatedAt = DateTime.UtcNow;
+    }
+}

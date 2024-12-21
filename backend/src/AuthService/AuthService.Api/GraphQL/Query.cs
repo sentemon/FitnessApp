a4905bb947b3e10bbaf4eba5279ff01c7f896544
Ivan Sentemon <sentemon.i@gmail.com>
2024-12-21 09:41:37 +0100

    Add GetUserById query to GraphQL API

--- backend/src/AuthService/AuthService.Api/GraphQL/Query.cs
+++ backend/src/AuthService/AuthService.Api/GraphQL/Query.cs
@@ -1,6 +1,29 @@
+using System.Security.Claims;
+using AuthService.Application.DTOs;
+using AuthService.Application.Queries.GetUserById;
+
 namespace AuthService.Api.GraphQL;
 
 public class Query
 {
-    
+    private readonly IHttpContextAccessor _httpContextAccessor;
+
+    public Query(IHttpContextAccessor httpContextAccessor)
+    {
+        _httpContextAccessor = httpContextAccessor;
+    }
+
+    public async Task<UserDto> GetUserById([Service] GetUserByIdQueryHandler getUserByIdQueryHandler)
+    {
+        var userId = _httpContextAccessor.HttpContext?.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+        var query = new GetUserByIdQuery(userId);
+        var result = await getUserByIdQueryHandler.HandleAsync(query);
+
+        if (!result.IsSuccess)
+        {
+            throw new GraphQLException(new Error(result.Error.Message));
+        }
+
+        return result.Response;
+    }
 }

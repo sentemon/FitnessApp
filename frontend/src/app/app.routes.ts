a92e99d246b82127758f69b19d88fdc680b8a5d9
Ivan Sentemon <sentemon.i@gmail.com>
2025-01-03 17:51:15 +0100

    add to styling login component

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import {AppComponent} from "./app.component";
+import {LoginComponent} from "./features/auth/componets/login/login.component";
 
 export const routes: Routes = [
   // { path: '', component: AppComponent, canActivate: [AuthGuard] },
+  { path: 'login', component: LoginComponent },
   { path: '**', redirectTo: '', pathMatch: 'full' }
 ];
--- frontend/src/app/features/auth/componets/login/login.component.ts
+++ frontend/src/app/features/auth/componets/login/login.component.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
+import {NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    NgIf
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  loginForm: FormGroup;
 
+  constructor(private formBuilder: FormBuilder) {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    })
+  }
+  onLogin() {
+    if (this.loginForm.valid) {
+      // ToDo
+      console.log('Form Submitted:', this.loginForm.value);
+    } else {
+      // ToDo
+      console.log('Form is invalid', this.loginForm.errors);
+    }
+  }
 }

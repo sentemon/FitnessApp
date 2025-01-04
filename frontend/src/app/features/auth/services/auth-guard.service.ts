2f5c40b12a7b5d2d43fcbd5729e1a7d70472e8db
Ivan Sentemon <sentemon.i@gmail.com>
2025-01-04 11:40:14 +0100

    Refactor token management

--- frontend/src/app/features/auth/services/auth-guard.service.ts
+++ frontend/src/app/features/auth/services/auth-guard.service.ts
@@ -1,32 +1,48 @@
 import { Injectable } from '@angular/core';
 import {Apollo} from "apollo-angular";
-import {map, Observable} from "rxjs";
+import { map } from "rxjs";
 import {LOGIN, REGISTER} from "../requests/mutations";
 import {Token} from "../../../core/models/token.model";
 import {TokenResponse} from "../responses/token.response";
+import {TokenService} from "../../../core/services/token.service";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuardService {
+  private token?: Token;
 
-  constructor(private apollo: Apollo) { }
+  constructor(private apollo: Apollo, private tokenService: TokenService) { }
 
-  public login(username: string, password: string) : Observable<Token | undefined> {
-    return this.apollo.mutate<TokenResponse>({
+  public login(username: string, password: string): void {
+    this.apollo.mutate<TokenResponse>({
       mutation: LOGIN,
       variables: { username, password }
     }).pipe(
       map(response => response.data?.login)
-    );
+    ).subscribe(result => {
+      this.token = result;
+    });
+
+    this.tokenService.set(this.token);
   }
 
-  public register(firstName: string, lastName: string, username: string, email: string, password: string) : Observable<Token | undefined> {
-    return this.apollo.mutate<TokenResponse>({
+  public register(
+    firstName: string,
+    lastName: string,
+    username: string,
+    email: string,
+    password: string
+  ): void {
+    this.apollo.mutate<TokenResponse>({
       mutation: REGISTER,
       variables: { firstName, lastName, username, email, password }
     }).pipe(
       map(response => response.data?.register)
-    );
+    ).subscribe(result => {
+      this.token = result;
+    });
+
+    this.tokenService.set(this.token);
   }
 }

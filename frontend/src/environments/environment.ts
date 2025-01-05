7897f305adfd3469f15781753de08d0c9d6a8053
Ivan Sentemon <sentemon.i@gmail.com>
2025-01-05 19:43:13 +0100

    Update service ports and environment configurations

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
-  auth_service: "http://localhost:8082/graphql",
+  auth_service: "http://localhost:8000/auth/graphql",
+  post_service: "http://localhost:8000/post/",
   production: false
 };

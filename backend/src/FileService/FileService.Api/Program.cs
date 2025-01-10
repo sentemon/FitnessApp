f97bc9b6b0fe9c43a1a50748ffbb13c480d8efda
Ivan Sentemon <sentemon.i@gmail.com>
2025-01-10 08:29:32 +0100

    Add dependency injection and project references for FileService components

--- backend/src/FileService/FileService.Api/Program.cs
+++ backend/src/FileService/FileService.Api/Program.cs
@@ -1,4 +1,13 @@
+using FileService.Application;
+using FileService.Infrastructure;
+using FileService.Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services
+    .AddPersistenceServices(builder.Configuration)
+    .AddInfrastructureServices(builder.Configuration)
+    .AddApplicationServices();
 
 var app = builder.Build();
 
---
+++ backend/src/FileService/FileService.Domain/Constants/AppSettingsConstants.cs
@@ -0,0 +1,6 @@
+namespace FileService.Domain.Constants;
+
+public class AppSettingsConstants
+{
+    public const string AzuriteAccounts = "AZURITE_ACCOUNTS";
+}

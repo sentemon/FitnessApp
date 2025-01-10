164740c9b7dfaba547ec4c03606ce18d9bde789d
Ivan Sentemon <sentemon.i@gmail.com>
2025-01-10 11:39:34 +0100

    Configure hosting URL

--- backend/src/FileService/FileService.Api/Program.cs
+++ backend/src/FileService/FileService.Api/Program.cs
@@ -1,9 +1,14 @@
 using FileService.Application;
+using FileService.Domain.Constants;
 using FileService.Infrastructure;
 using FileService.Persistence;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+
+var hostingUrl = builder.Configuration[AppSettingsConstants.WebHostUrl];
+
+builder.WebHost.UseUrls(hostingUrl ?? throw new ArgumentNullException(nameof(hostingUrl), "Hosting URL is not configured."));
 
 builder.Services
     .AddPersistenceServices(builder.Configuration)
@@ -17,5 +22,7 @@ using (var scope = app.Services.CreateScope())
     var dbContext = scope.ServiceProvider.GetRequiredService<FileDbContext>();
     dbContext.Database.Migrate();
 }
+
+app.MapGet("/health", () => Results.Ok("Healthy"));
 
 app.Run();
--- backend/src/FileService/FileService.Domain/Constants/AppSettingsConstants.cs
+++ backend/src/FileService/FileService.Domain/Constants/AppSettingsConstants.cs
@@ -2,6 +2,7 @@ namespace FileService.Domain.Constants;
 
 public static class AppSettingsConstants
 {
+    public const string WebHostUrl = "WebHostUrl";
     public const string DatabaseConnectionString = "DatabaseConnectionString";
     public const string AzureStorageConnectionString = "AzureStorageConnectionString";
 }

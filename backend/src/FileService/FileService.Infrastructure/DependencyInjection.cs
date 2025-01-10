354188417e2e8e955af81544a889b3e8f231697e
Ivan Sentemon <sentemon.i@gmail.com>
2025-01-10 12:10:13 +0100

    Fix Azure Storage connection string and update Docker Compose for file service routing

--- backend/src/FileService/FileService.Infrastructure/DependencyInjection.cs
+++ backend/src/FileService/FileService.Infrastructure/DependencyInjection.cs
@@ -9,7 +9,9 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
     {
-        services.AddSingleton(new BlobServiceClient(configuration.GetConnectionString(AppSettingsConstants.AzureStorageConnectionString)));
+        var azureStorageConnectionString = configuration.GetConnectionString(AppSettingsConstants.AzureStorageConnectionString);
+        
+        services.AddSingleton(new BlobServiceClient(azureStorageConnectionString));
         
         return services;
     }

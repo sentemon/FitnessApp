ba9fbb17f9ba7798fa01a1f1b87193575b75db74
Ivan Sentemon <sentemon.i@gmail.com>
2025-01-23 11:43:27 +0100

    Enhance setup profile component with step navigation and form improvements

--- frontend/src/app/features/workouts/workouts.module.ts
+++ frontend/src/app/features/workouts/workouts.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { SetUpProfileComponent } from './components/set-up-profile/set-up-profile.component';
 import { StepperModule } from 'primeng/stepper';
-import {NgClass} from "@angular/common";
+import {NgClass, NgIf, NgStyle} from "@angular/common";
 import {Button} from "primeng/button";
 import {FormsModule} from "@angular/forms";
 import {Password} from "primeng/password";
+import {Steps} from "primeng/steps";
 
 @NgModule({
   declarations: [
@@ -12,10 +13,9 @@ import {Password} from "primeng/password";
   ],
   imports: [
     StepperModule,
-    NgClass,
-    Button,
-    FormsModule,
-    Password
+    Steps,
+    NgIf,
+    NgStyle
   ]
 })
 export class WorkoutsModule { }

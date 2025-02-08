c88e7bb75a3cf630c4327933a1db367f4c920e36
Ivan Sentemon <sentemon.i@gmail.com>
2025-02-08 13:37:47 +0100

    Add unit tests for SetHistory marking as completed and uncompleted

--- backend/tests/WorkoutService/WorkoutService.Domain.Tests/SetTests.cs
+++ backend/tests/WorkoutService/WorkoutService.Domain.Tests/SetTests.cs
@@ -23,39 +23,4 @@ public class SetTests
         set.Weight.Should().Be(weight);
         set.ExerciseId.Should().Be(exerciseId);
     }
-
-    // [Fact]
-    // public void MarkAsCompleted_ShouldMarkPropertyCompletedAsTrue()
-    // {
-    //     // Arrange
-    //     var reps = 12u;
-    //     var weight = 30;
-    //     var exerciseId = Exercise.Create("Example Exercise", DifficultyLevel.Beginner, Guid.NewGuid().ToString()).Id;
-    //     
-    //     var set = Set.Create(reps, weight, exerciseId);
-    //     
-    //     // Act
-    //     set.MarkAsCompleted();
-    //     
-    //     // Assert
-    //     set.Completed.Should().BeTrue();
-    // }
-    //
-    // [Fact]
-    // public void MarkAsUncompleted_ShouldMarkPropertyCompletedAsFalse()
-    // {
-    //     // Arrange
-    //     var reps = 12u;
-    //     var weight = 30;
-    //     var exerciseId = Exercise.Create("Example Exercise", DifficultyLevel.Beginner, Guid.NewGuid().ToString()).Id;
-    //     
-    //     var set = Set.Create(reps, weight, exerciseId);
-    //     set.MarkAsCompleted();
-    //     
-    //     // Act
-    //     set.MarkAsUncompleted();
-    //     
-    //     // Assert
-    //     set.Completed.Should().BeFalse();
-    // }
 }

c66c3b593532392805b0ca0851cdf30e9dbdbfd7
Ivan Sentemon <sentemon.i@gmail.com>
2025-02-15 09:30:02 +0100

    Add unit tests for adding and deleting sets in Exercise class

--- backend/tests/WorkoutService/WorkoutService.Domain.Tests/ExerciseTests.cs
+++ backend/tests/WorkoutService/WorkoutService.Domain.Tests/ExerciseTests.cs
@@ -24,4 +24,41 @@ public class ExerciseTests
         exercise.UserId.Should().Be(userId);
         exercise.Sets.Should().BeEmpty();
     }
+
+    [Fact]
+    public void AddSet_ShouldAddSetCorrectly()
+    {
+        // Arrange
+        var name = "Example Exercise";
+        var level = DifficultyLevel.Beginner;
+        var userId = Guid.NewGuid().ToString();
+        var exercise = Exercise.Create(name, level, userId);
+        var set = Set.Create(35, 40, exercise.Id);
+        
+        // Act
+        exercise.AddSet(set);
+        
+        // Assert
+        exercise.Sets.Count.Should().Be(1);
+        exercise.Sets.Should().Contain(set);
+    }
+    
+    [Fact]
+    public void DeleteSet_ShouldDeleteSetCorrectly()
+    {
+        // Arrange
+        var name = "Example Exercise";
+        var level = DifficultyLevel.Beginner;
+        var userId = Guid.NewGuid().ToString();
+        var exercise = Exercise.Create(name, level, userId);
+        var set = Set.Create(35, 40, exercise.Id);
+        exercise.AddSet(set);
+        
+        // Act
+        exercise.DeleteSet(set);
+        
+        // Assert
+        exercise.Sets.Should().BeEmpty();
+        exercise.Sets.Should().NotContain(set);
+    }
 }

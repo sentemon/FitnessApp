701b9817f2aeb245af8860fef9906d1f12a0fa2b
Ivan Sentemon <sentemon.i@gmail.com>
2025-02-18 17:30:59 +0100

    Add unit tests for workout and exercise management in User class

--- backend/tests/WorkoutService/WorkoutService.Domain.Tests/UserTests.cs
+++ backend/tests/WorkoutService/WorkoutService.Domain.Tests/UserTests.cs
@@ -8,7 +8,7 @@ namespace WorkoutService.Domain.Tests;
 public class UserTests
 {
     [Fact]
-    public async Task SetUpProfile_ShouldSetUpProfileCorrectly()
+    public void SetUpProfile_ShouldSetUpProfileCorrectly()
     {
         // Arrange
         var user = User.Create(Guid.NewGuid().ToString(), "Example", "Example", "example", string.Empty);
@@ -30,5 +30,100 @@ public class UserTests
         user.ActivityLevel.Should().Be(activityLevel);
         user.FavoriteWorkoutTypes.Should().Equal(favoriteWorkoutTypes);
         user.DateOfBirth.Should().Be(dateOfBirth);
+    }
+
+    [Fact]
+    public void AddWorkout_ShouldAddWorkoutCorrectly()
+    {
+        // Arrange
+        var id = Guid.NewGuid().ToString();
+        var user = User.Create(id, "Example", "Example", "example", string.Empty);
+        var workout = Workout.Create("Example Title", "Example Description", 30u, DifficultyLevel.Beginner, id);
+        
+        // Act
+        user.AddWorkout(workout);
+        
+        // Assert
+        user.Workouts.Should().Contain(workout);
+    }
+    
+    [Fact]
+    public void DeleteWorkout_ShouldDeleteWorkoutCorrectly()
+    {
+        // Arrange
+        var id = Guid.NewGuid().ToString();
+        var user = User.Create(id, "Example", "Example", "example", string.Empty);
+        var workout = Workout.Create("Example Title", "Example Description", 30u, DifficultyLevel.Beginner, id);
+        user.AddWorkout(workout);
+        
+        // Act
+        user.DeleteWorkout(workout);
+        
+        // Assert
+        user.Workouts.Should().NotContain(workout);
+    }
+    
+    [Fact]
+    public void AddExercise_ShouldAddExerciseCorrectly()
+    {
+        // Arrange
+        var id = Guid.NewGuid().ToString();
+        var user = User.Create(id, "Example", "Example", "example", string.Empty);
+        var exercise = Exercise.Create("Example Title", DifficultyLevel.Beginner, id);
+        
+        // Act
+        user.AddExercise(exercise);
+        
+        // Assert
+        user.Exercises.Should().Contain(exercise);
+    }
+
+    [Fact]
+    public void DeleteExercise_ShouldDeleteExerciseCorrectly()
+    {
+        // Arrange
+        var id = Guid.NewGuid().ToString();
+        var user = User.Create(id, "Example", "Example", "example", string.Empty);
+        var exercise = Exercise.Create("Example Title", DifficultyLevel.Beginner, id);
+        user.AddExercise(exercise);
+        
+        // Act
+        user.DeleteExercise(exercise);
+        
+        // Assert
+        user.Exercises.Should().NotContain(exercise);
+    }
+    
+    [Fact]
+    public void AddWorkoutHistory_ShouldAddWorkoutHistoryCorrectly()
+    {
+        // Arrange
+        var id = Guid.NewGuid().ToString();
+        var user = User.Create(id, "Example", "Example", "example", string.Empty);
+        var workoutId = Workout.Create("Example Title", "Example Description", 30u, DifficultyLevel.Beginner, id).Id;
+        var workoutHistory = WorkoutHistory.Create(30u, workoutId, id);
+        
+        // Act
+        user.AddWorkoutHistory(workoutHistory);
+        
+        // Assert
+        user.WorkoutHistories.Should().Contain(workoutHistory);
+    }
+    
+    [Fact]
+    public void DeleteWorkoutHistory_ShouldDeleteWorkoutHistoryCorrectly()
+    {
+        // Arrange
+        var id = Guid.NewGuid().ToString();
+        var user = User.Create(id, "Example", "Example", "example", string.Empty);
+        var workoutId = Workout.Create("Example Title", "Example Description", 30u, DifficultyLevel.Beginner, id).Id;
+        var workoutHistory = WorkoutHistory.Create(30u, workoutId, id);
+        user.AddWorkoutHistory(workoutHistory);
+        
+        // Act
+        user.DeleteWorkoutHistory(workoutHistory);
+        
+        // Assert
+        user.WorkoutHistories.Should().NotContain(workoutHistory);
     }
 }

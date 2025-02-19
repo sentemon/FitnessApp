3185e0cf7013334fcb3bbb735539679e02aac8aa
Ivan Sentemon <sentemon.i@gmail.com>
2025-02-19 12:16:54 +0100

    Add tests for WorkoutHistory

---
+++ backend/tests/WorkoutService/WorkoutService.Domain.Tests/WorkoutHistoryTests.cs
@@ -0,0 +1,42 @@
+using FluentAssertions;
+using WorkoutService.Domain.Entities;
+using WorkoutService.Domain.Enums;
+using Xunit;
+
+namespace WorkoutService.Domain.Tests;
+
+public class WorkoutHistoryTests
+{
+    [Fact]
+    public void Create_ShouldCreateWorkoutHistoryCorrectly()
+    {
+        // Arrange
+        var durationInMinutes = 45u;
+        var workoutId = Guid.NewGuid();
+        var userId = Guid.NewGuid().ToString();
+
+        // Act
+        var workoutHistory = WorkoutHistory.Create(durationInMinutes, workoutId, userId);
+
+        // Assert
+        workoutHistory.WorkoutId.Should().Be(workoutId);
+        workoutHistory.ExerciseHistories.Should().BeEmpty();
+        workoutHistory.UserId.Should().Be(userId);
+    }
+
+    [Fact]
+    public void AddExerciseHistory_ShouldAddExerciseHistoryCorrectly()
+    {
+        // Arrange
+        var userId = Guid.NewGuid().ToString();
+        var workoutHistory = WorkoutHistory.Create(45u, Guid.NewGuid(), userId);
+        var exercise = Exercise.Create("Example", DifficultyLevel.Intermediate, userId);
+        var exerciseHistory = ExerciseHistory.Create(workoutHistory.Id, exercise.Id);
+        
+        // Act
+        workoutHistory.AddExerciseHistory(exerciseHistory);
+        
+        // Assert
+        workoutHistory.ExerciseHistories.Should().Contain(exerciseHistory);
+    }
+}

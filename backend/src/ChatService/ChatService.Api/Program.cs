c52c29093b4be9d8cdb64c480f4216c196a95403
Ivan Sentemon <sentemon.i@gmail.com>
2025-03-17 21:39:41 +0100

    Add Application, Infrastructure, Persistence, Domain projects

--- backend/src/ChatService/ChatService.Api/Program.cs
+++ backend/src/ChatService/ChatService.Api/Program.cs
@@ -1,4 +1,13 @@
+using ChatService.Application;
+using ChatService.Infrastructure;
+using ChatService.Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services
+    .AddPersistenceServices()
+    .AddInfrastructureServices()
+    .AddApplicationServices();
 
 var app = builder.Build();
 

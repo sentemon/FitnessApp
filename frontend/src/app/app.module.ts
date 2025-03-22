325902dbe22bd633a19d5b5a18195cabaf5b2940
Ivan Sentemon <sentemon.i@gmail.com>
2025-03-22 08:41:33 +0100

    Add ChatComponent and update routing for chat feature

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -15,12 +15,14 @@ import {AuthInterceptor} from "./core/services/auth.interceptor";
 import {WorkoutsModule} from "./features/workouts/workouts.module";
 import { ChatSidebarComponent } from './features/chats/components/chat-sidebar/chat-sidebar.component';
 import { ChatAreaComponent } from './features/chats/components/chat-area/chat-area.component';
+import { ChatComponent } from './features/chats/components/chat/chat.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ChatSidebarComponent,
-    ChatAreaComponent
+    ChatAreaComponent,
+    ChatComponent
   ],
   imports: [
     BrowserModule,

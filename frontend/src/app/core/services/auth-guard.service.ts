477dc06bc28015fef8411e6af85e96eea072aea8
Ivan Sentemon <sentemon.i@gmail.com>
2025-03-29 19:17:49 +0100

    Refactor chat components and styles for improved layout and structure

--- frontend/src/app/core/services/auth-guard.service.ts
+++ frontend/src/app/core/services/auth-guard.service.ts
@@ -20,7 +20,7 @@ export class AuthGuard implements CanActivate {
         if (result) {
           return true;
         }
-        
+
         this.router.navigate(["login"])
         return false;
       })

045085326133f5ae6725ea2902f68a0dcec20b6f
Ivan Sentemon <sentemon.i@gmail.com>
2025-04-06 12:34:28 +0200

    Refactoring to result extension

--- frontend/src/app/core/extensions/graphql-result-wrapper.ts
+++ frontend/src/app/core/extensions/graphql-result-wrapper.ts
@@ -1,26 +1,29 @@
 import { ApolloError } from '@apollo/client/core';
-import { Result } from "../types/result/result";
+import { Result } from '../types/result/result';
+import { Observable, of } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
 
-export async function toResult<T>(promise: Promise<any>): Promise<Result<T>> {
-  try {
-    const result = await promise;
+export function toResult<T>(source$: Observable<any>): Observable<Result<T>> {
+  return source$.pipe(
+    map(result => {
+      if (result.errors?.length) {
+        const gqlError = result.errors[0];
+        return Result.failure<T>(new Error(gqlError.message));
+      }
 
-    if (result.errors.length) {
-      const gqlError = result.errors[0];
-      return Result.failure<T>(new Error(gqlError.message));
-    }
+      if (result.data) {
+        return Result.success<T>(result.data as T);
+      }
 
-    if (result.data) {
-      return Result.success<T>(result.data);
-    }
+      return Result.failure<T>(new Error('Unknown error'));
+    }),
+    catchError((err: any) => {
+      if (err instanceof ApolloError) {
+        const message = err.message || 'Apollo error';
+        return of(Result.failure<T>(new Error(message)));
+      }
 
-    return Result.failure<T>(new Error('Unknown error'));
-  } catch (err: any) {
-    if (err instanceof ApolloError) {
-      const message = err.message || 'Apollo error';
-      return Result.failure<T>(new Error(message));
-    }
-
-    return Result.failure<T>(new Error('Unexpected error'));
-  }
+      return of(Result.failure<T>(new Error('Unexpected error')));
+    })
+  );
 }

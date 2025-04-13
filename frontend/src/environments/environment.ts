2294fac0b65f701a5f8971da96873d1035a33dcb
Ivan Sentemon <sentemon.i@gmail.com>
2025-04-13 09:41:25 +0200

    Add Chat Service endpoint to environment configuration

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -2,5 +2,6 @@ export const environment = {
   auth_service: "http://localhost:8000/auth/graphql",
   post_service: "http://localhost:8000/post/graphql",
   workout_service: "http://localhost:8000/workout/graphql",
+  chat_service: "http://localhost:8000/chat/graphql",
   production: false
 };

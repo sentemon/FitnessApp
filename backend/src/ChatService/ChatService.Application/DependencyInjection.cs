f4bfb39f3186670c78194d1c710b5eab59c7829b
Ivan Sentemon <sentemon.i@gmail.com>
2025-04-14 12:42:27 +0200

    Add CreateChat command and handler, update project references, and configure dependency injection

--- backend/src/ChatService/ChatService.Application/DependencyInjection.cs
+++ backend/src/ChatService/ChatService.Application/DependencyInjection.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
+using Shared.Application.Extensions;
 
 namespace ChatService.Application;
 
@@ -6,6 +8,9 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddApplicationServices(this IServiceCollection services)
     {
+        services.AddQueryHandlers(Assembly.GetExecutingAssembly());
+        services.AddCommandHandlers(Assembly.GetExecutingAssembly());
+        
         return services;
     }
 }

cc8d510baa14f0833aba6e19c3c6bbbd2d4cbe2a
Ivan Sentemon <sentemon.i@gmail.com>
2025-06-05 21:37:51 +0200

    Refactor profile component to improve user data display and structure

--- frontend/src/app/features/auth/componets/profile/profile.component.ts
+++ frontend/src/app/features/auth/componets/profile/profile.component.ts
@@ -1,24 +1,38 @@
-import {Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {UserService} from "../../services/user.service";
 import {User} from "../../models/user.model";
+import {PostService} from "../../../posts/services/post.service";
+import {Post} from "../../../posts/models/post.model";
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss'
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit {
   user!: User;
+  posts: Post[] = [];
 
-  constructor(private userService: UserService) { }
+  constructor(private userService: UserService, private postService: PostService) { }
 
-  getSentemon(): void {
-    this.userService.getUserByUsername("sentemon").subscribe(result => {
+  ngOnInit() {
+    this.userService.getCurrentUser().subscribe(result => {
       if (result.isSuccess) {
         this.user = result.response;
+        this.getPosts();
       } else {
-        console.log(result.error.message)
+        console.warn(result.error.message);
       }
-    });
+    })
+  }
+
+  private getPosts() {
+    this.postService.getAllPosts().subscribe(result => {
+      if (result.isSuccess) {
+        this.posts = result.response;
+      } else {
+        console.warn(result.error.message);
+      }
+    })
   }
 }

71a0c7dc19725fd2093d0878144fec4f1bf6aab7
Ivan Sentemon <sentemon.i@gmail.com>
2025-07-19 17:27:59 +0200

    Add production environment configuration and update file replacements in angular.json

---
+++ frontend/src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  auth_service: "https://api.sentemon.me/auth/graphql",
+  post_service: "https://api.sentemon.me/post/graphql",
+  workout_service: "https://api.sentemon.me/workout/graphql",
+  chat_service: "https://api.sentemon.me/chat/graphql",
+  production: true
+};

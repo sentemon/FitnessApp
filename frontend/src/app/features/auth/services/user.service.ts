5b33d43d2ae4c885e8bfa48661923ba0303b78be
Ivan Sentemon <sentemon.i@gmail.com>
2025-08-10 13:18:02 +0200

    Add DELETE_USER mutation and implement deleteUser method in UserService

--- frontend/src/app/features/auth/services/user.service.ts
+++ frontend/src/app/features/auth/services/user.service.ts
@@ -13,7 +13,7 @@ import {User} from "../models/user.model";
 import {toResult} from "../../../core/extensions/graphql-result-wrapper";
 import {Result} from "../../../core/types/result/result.type";
 import {UserDto} from "../models/user-dto.model";
-import {FOLLOW, UNFOLLOW} from "../graphql/mutations";
+import {DELETE_USER, FOLLOW, UNFOLLOW} from "../graphql/mutations";
 
 @Injectable({
   providedIn: 'root'
@@ -93,6 +93,14 @@ export class UserService {
       variables: { targetUserId }
     }).pipe(
       toResult<boolean>('isFollowing')
+    );
+  }
+
+  deleteUser(): Observable<Result<boolean>> {
+    return this.authClient.mutate({
+      mutation: DELETE_USER
+    }).pipe(
+      toResult<boolean>('deleteUser')
     );
   }
 }

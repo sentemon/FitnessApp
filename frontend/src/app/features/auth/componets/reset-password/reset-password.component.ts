c800ffd00c0d760c87c87e18336b3f71fca43b62
Ivan Sentemon <sentemon.i@gmail.com>
2025-08-18 17:16:11 +0200

    Implement ResetPassword form with validation and submission logic

--- frontend/src/app/features/auth/componets/reset-password/reset-password.component.ts
+++ frontend/src/app/features/auth/componets/reset-password/reset-password.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {UserService} from "../../services/user.service";
+import {AbstractControl, FormBuilder, FormGroup, ValidationErrors, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-reset-password',
@@ -7,5 +8,33 @@ import {UserService} from "../../services/user.service";
   styleUrl: './reset-password.component.scss'
 })
 export class ResetPasswordComponent {
-  constructor(private userService: UserService) { }
+  resetPasswordForm: FormGroup;
+
+  constructor(private userService: UserService, private formBuilder: FormBuilder) {
+    this.resetPasswordForm = formBuilder.group({
+      oldPassword: ['', Validators.required, Validators.minLength(6)],
+      newPassword: ['', Validators.required, Validators.minLength(6)],
+      confirmNewPassword: ['', Validators.required, Validators.minLength(6)]
+    }, { validators: this.passwordMatchValidator });
+  }
+
+  onResetPassword() {
+    this.userService.resetPassword(
+      this.resetPasswordForm.get("oldPassword")?.value,
+      this.resetPasswordForm.get("newPassword")?.value,
+      this.resetPasswordForm.get("confirmNewPassword")?.value
+    ).subscribe(result => {
+      console.log(result);
+      if (!result.isSuccess) {
+        console.log(result.error.message);
+      }
+    });
+  }
+
+  private passwordMatchValidator(group: AbstractControl): ValidationErrors | null {
+    const newPassword = group.get('newPassword')?.value;
+    const confirmNewPassword = group.get('confirmNewPassword')?.value;
+    return newPassword === confirmNewPassword ? null : { passwordMismatch: true };
+  }
+
 }
